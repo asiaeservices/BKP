6540833e7d6f18132f6e6e6cbfb7faa64e774992
asep.mylocal <asep_mylocal@yahoo.com>
2011-05-11 17:19:23 +0700

    ubah comment di index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// change the following paths if necessary
+// change the following paths if necessary ini yang diubah
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 //$yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
